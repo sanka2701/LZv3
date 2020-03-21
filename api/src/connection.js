7d4353d60ce64641eec280fa9a1440af9be25490
sanka2701 <husenica.jan@gmail.com>
2020-03-21 11:02:04 +0100

    structure change

--- api/src/connection.js
+++ api/src/connection.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 const User = require("./User.model");
 
-const connection = "mongodb://mongo:27017/mongo-test";
+const connection = "mongodb://mongo:27017/liptov-zije";
 
 const connectDb = () => {
   return mongoose.connect(connection);
